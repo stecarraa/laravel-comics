63d1bf8c6187a0ac6351d7a405037365bdc923bc
stecarraa <stefano@caratozzolo.me>
2022-09-07 18:04:10 +0200

    fix: content ok css card not works properly

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,19 +3,29 @@
 @section("main-content")
 
     <div>
-        @foreach ($comics as $comic)
-            <div class="card">
 
-            <div class="comic-img-container">
-                <img src="{{ $comic->image }}" alt="{{ $comic->title }}">
-            </div>
-            <h4>{{ $comic->title }}</h4>
 
-            </div>       
-        @endforeach
+        <div class="p-5 bg-black text-center position-relative ">
+
+            <button class="ms-position btn btn-primary text-uppercase "> Current series</button>
+              <div class="d-flex flex-wrap">
+                @foreach ($comics as $comic)
+                <div class="card">
+    
+                <div class="comic-img-container">
+                    <img src="{{ $comic->image }}" alt="{{ $comic->title }}">
+                </div>
+                <h4>{{ $comic->title }}</h4>
+    
+                </div>       
+            @endforeach
+          </div>
+      
+          <button class="btn btn-primary btn-lg">Load more</button>
+          </div>
+
+       
     </div>
-    <div>
-        <a href="#">load more</a>
-    </div>
+    
 
 @endsection
---
+++ resources/views/includes/footer.blade.php
@@ -0,0 +1,74 @@
+<footer>
+    <div class="main-footer-container overflow-hidden ">
+        <div class="container p-3">
+            <div class="row d-flex h-100 align-items-center">
+                <div class="col-6 text-white h-100 d-flex mt-2">
+                    <div class="col-4">
+                                <h6>DC COMICS</h6>
+                                <ul class="list-unstyled text-secondary">
+                                    <li>Item</li>
+                                    <li>Item</li>
+                                    <li>Item</li>
+                                    <li>Item</li>
+                                    <li>Item</li>
+                                    <li>Item</li>
+                                    <li>Item</li>
+                                    <li>Item</li>
+                                </ul>
+                                <h6>SHOP</h6>
+                                <ul class="list-unstyled text-secondary">
+                                    <li>Item</li>
+                                    <li>Item</li>
+                                </ul>
+                            </div>
+                            <div class="col-4">
+                                <h6>DC COMICS</h6>
+                                <ul class="list-unstyled text-secondary">
+                                    <li>Item</li>
+                                    <li>Item</li>
+                                    <li>Item</li>
+                                    <li>Item</li>
+                                    <li>Item</li>
+                                    <li>Item</li>
+                                    <li>Item</li>
+                                    <li>Item</li>
+                                    <li>Item</li>
+                                    <li>Item</li>
+                                    <li>Item</li>
+                                </ul>
+                            </div>
+                            <div class="col-4">
+                                <h6>DC COMICS</h6>
+                                <ul class="list-unstyled text-secondary">
+                                    <li>Item</li>
+                                    <li>Item</li>
+                                    <li>Item</li>
+                                    <li>Item</li>
+                                </ul>
+                            </div></div>
+                <div class="col-6 p-5 position-relative"><img class="position-absolute  top-50 start-50 translate-middle" src="../assets/img/dc-logo-bg.png" alt=""></div>
+            </div>
+        </div>
+    </div>
+    <div class="container-fluid ms-lower-footer-bg">
+        <div class="container">
+            
+              <div class="row d-flex align-items-center">
+            <div class="col-6">
+                <button class="btn  btn-outline-primary text-white border-3">Sign-up NOW</button>
+            </div>
+            <div class="col-6 d-flex align-items-center justify-content-end">
+                <div class="text-uppercase ms-main-blue-color fw-bold">follow us</div>
+                
+                <ul class="list-unstyled d-flex">
+                
+                                    <li class="m-3"><img src="../assets/img/footer-facebook.png" alt=""></li>
+                                    <li class="m-3"><img src="../assets/img/footer-periscope.png" alt=""></li>
+                                    <li class="m-3"><img src="../assets/img/footer-pinterest.png" alt=""></li>
+                                    <li class="m-3"><img src="../assets/img/footer-youtube.png" alt=""></li>
+                                    <li class="m-3"><img src="../assets/img/footer-twitter.png" alt=""></li>
+                                </ul></div>
+        </div>
+        </div>
+    </div>
+</footer>
